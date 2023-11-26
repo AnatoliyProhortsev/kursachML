a8b48091a4a09a1a682da356ed80107513675560
AnatoliyProhortsev <tolik.spb.ru@yandex.ru>
2023-11-26 17:42:45 +0300

    skleika i bez

--- ex.cpp
+++ ex.cpp
@@ -36,211 +36,174 @@ int main(void)
 
 	bdd task = bddtrue;
 	//1
-	task &= p[0][2][7];
-	task &= p[2][4][8];
-	task &= p[0][5][4];
-	task &= p[3][5][0];
-	task &= p[1][8][8];
-	task &= p[1][4][1];
-	task &= p[2][5][1];
+	task &= p[3][0][5];
+	task &= p[3][5][8];
+	task &= p[2][2][6];
+	task &= p[3][2][4];
+	task &= p[2][0][0];
+	task &= p[1][8][0];
+	task &= p[3][3][1];
 
 	//1-доп
-	task &= p[3][4][5];
-	task &= p[0][7][1];
-	task &= p[2][3][3];
-	task &= p[2][7][7];
+	task &= p[1][1][4];
+	task &= p[1][3][7];
+	task &= p[0][8][2];
+	task &= p[0][5][7];
+	task &= p[2][3][1];
+	task &= p[1][7][1];
 
 	//2
 	for(unsigned i = 0; i < N; i++)
-		task &= !(p[1][i][6] ^ p[2][i][3]) ;
+		task &= !(p[2][i][6] ^ p[1][i][6]) ;
 	for(unsigned i = 0; i < N; i++)
-		task &= !(p[3][i][7] ^ p[1][i][6]) ;
+		task &= !(p[3][i][3] ^ p[0][i][3]) ;
 	for(unsigned i = 0; i < N; i++)
-		task &= !(p[3][i][6] ^ p[1][i][3]) ;
+		task &= !(p[0][i][6] ^ p[1][i][2]) ;
 	for(unsigned i = 0; i < N; i++)
-		task &= !(p[2][i][3] ^ p[1][i][6]) ;
+		task &= !(p[3][i][7] ^ p[0][i][5]) ;
 	for(unsigned i = 0; i < N; i++)
-		task &= !(p[3][i][8] ^ p[0][i][2]) ;
+		task &= !(p[0][i][7] ^ p[2][i][7]) ;
 
 	//2-доп
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[0][i][3] ^ p[3][i][2]) ;
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[0][i][5] ^ p[1][i][8]) ;
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[2][i][2] ^ p[1][i][2]) ;
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[3][i][5] ^ p[0][i][0]) ;
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[2][i][1] ^ p[0][i][4]) ;
-	// for(unsigned i = 0; i < N; i++)
-	// 	task &= !(p[2][i][7] ^ p[3][i][3]) ;
-
+	//-----
 
 	//3
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i != 2 && i != 5 && i != 8)
-			task &= !(p[1][i][3] ^ p[2][i+1][6]) ;
+			task &= !(p[3][i][0] ^ p[1][i+1][1]) ;
 		else
-			task &= !(p[1][i][3]);
+			task &= !(p[3][i][0]);
+	}
+	for(unsigned i = 0; i < N; i++)
+	{
+		if(i != 2 && i != 5 && i != 8)
+			task &= !(p[1][i][5] ^ p[1][i+1][4]) ;
+		else
+			task &= !(p[1][i][5]);
 	}
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[3][i][5] ^ p[2][i+2][0]) ;
+			task &= !(p[2][i][7] ^ p[2][i+2][4]) ;
 		else
-			task &= !(p[3][i][5]);
+			task &= !(p[2][i][7]);
 	}
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[3][i][8] ^ p[3][i+2][7]) ;
+			task &= !(p[1][i][8] ^ p[2][i+2][4]) ;
 		else
-			task &= !(p[3][i][8]);
+			task &= !(p[1][i][8]);
 	}
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[0][i][5] ^ p[1][i+2][5]) ;
+			task &= !(p[3][i][2] ^ p[3][i+2][1]) ;
 		else
-			task &= !(p[0][i][5]);
+			task &= !(p[3][i][2]);
 	}
-	for(unsigned i = 0; i < N; i++)
-	{
-		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[0][i][4] ^ p[3][i+2][6]) ;
-		else
-			task &= !(p[0][i][4]);
-	}
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		task &= !(p[3][i][1] ^ p[3][i+2][8]) ;
-	// 	else
-	// 		task &= !(p[3][i][1]);
-	// }
 
 	//3-Доп
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[2][i][4] ^ p[2][i+2][8]) ;
+			task &= !(p[0][i][7] ^ p[0][i+2][3]) ;
 		else
-			task &= !(p[2][i][4]);
+			task &= !(p[0][i][7]);
 	}
-	for(unsigned i = 0; i < N; i++)
-	{
-		if(i == 1 || i == 2 || i == 4 || i == 5)
-			task &= !(p[2][i][4] ^ p[3][i+2][5]) ;
-		else
-			task &= !(p[2][i][4]);
-	}
+
 
 	//4
 	bdd temp = bddfalse;
+	bool left = false, right = false;
+	for(unsigned i = 0; i < N; i++)
+	{
+		right = i != 2 && i != 5 && i != 8;
+		left = i == 1 || i == 2 || i == 4 || i == 5;
+		if(right)
+		{
+			temp |= !(p[2][i][5] ^ p[0][i+1][4]) ;
+		}
+		if(left)
+		{
+			temp |= !(p[2][i][5] ^ p[0][i+2][4]) ;
+		}
+		if(!right && !left)
+		{
+			task &= !(p[2][i][5]);
+		}
+		else
+		{
+			task &= temp;
+			temp = bddfalse;
+		}
+			
+	}
 	for(unsigned i = 0; i < N; i++)
 	{
 		if(i != 2 && i != 5 && i != 8)
 		{
-			temp |= !(p[0][i][0] ^ p[1][i+1][7]) ;
+			temp |= !(p[0][i][8] ^ p[3][i+1][1]) ;
 		}
 		if(i == 1 || i == 2 || i == 4 || i == 5)
 		{
-			temp |= !(p[0][i][0] ^ p[1][i+2][7]) ;
+			temp |= !(p[0][i][8] ^ p[3][i+2][1]) ;
 		}
 		if(!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5))
 		{
-			task &= !(p[0][i][0]);
+			task &= !(p[0][i][8]);
 		}
 		else
+		{
 			task &= temp;
-		temp = bddfalse;
+			temp = bddfalse;
+		}
 	}
-
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[0][i][8] ^ p[1][i+1][1]) ;
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[0][i][8] ^ p[1][i+2][1]) ;
-	// 	if(!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5))
-	// 		task &= !(p[0][i][8]);
-	// 	else
-	// 	{
-	// 		task &= temp;
-	// 	}
-	// }
-	
-	
-	// temp = bddfalse;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[3][i][4] ^ p[3][i+1][8]) ;
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[3][i][4] ^ p[3][i+2][8]) ;
-	// 	if((!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5)))
-	// 		task &= !(p[3][i][4]);
-	// }
-	// task &= temp;
-	// temp = bddfalse;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[1][i][3] ^ p[3][i+1][3]) ;
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[1][i][3] ^ p[3][i+2][3]) ;
-	// 	if((!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5)))
-	// 		task &= !(p[1][i][3]);
-	// }
-	// task &= temp;
-	// temp = bddfalse;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[2][i][1] ^ p[0][i+1][5]) ;
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[2][i][1] ^ p[0][i+2][5]) ;
-	// 	if(!(i != 2 && i != 5 && i != 8 || i == 1 || i == 2 || i == 4 || i == 5))
-	// 		task &= !(p[2][i][1]);
-	// }
-	// task &= temp;
-
+	for(unsigned i = 0; i < N; i++)
+	{
+		if(i != 2 && i != 5 && i != 8)
+		{
+			temp |= !(p[3][i][2] ^ p[0][i+1][0]) ;
+		}
+		if(i == 1 || i == 2 || i == 4 || i == 5)
+		{
+			temp |= !(p[3][i][2] ^ p[0][i+2][0]) ;
+		}
+		if(!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5))
+		{
+			task &= !(p[3][i][2]);
+		}
+		else
+		{
+			task &= temp;
+			temp = bddfalse;
+		}
+	}
+	for(unsigned i = 0; i < N; i++)
+	{
+		if(i != 2 && i != 5 && i != 8)
+		{
+			temp |= !(p[1][i][1] ^ p[1][i+1][0]) ;
+		}
+		if(i == 1 || i == 2 || i == 4 || i == 5)
+		{
+			temp |= !(p[1][i][1] ^ p[1][i+2][0]) ;
+		}
+		if(!(i != 2 && i != 5 && i != 8) && !(i == 1 || i == 2 || i == 4 || i == 5))
+		{
+			task &= !(p[1][i][1]);
+		}
+		else
+		{
+			task &= temp;
+			temp = bddfalse;
+		}
+	}
 	//4-доп
-	// temp = bddfalse;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[3][i][7] ^ p[1][i+1][0]) ;
-	// 	if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[3][i][7] ^ p[1][i+2][0]) ;
-	// 	if(!(i != 2 && i != 5 && i != 8 || i == 1 || i == 2 || i == 4 || i == 5))
-	// 		task &= !(p[3][i][7]);
-	// }
-	// task &= temp;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[0][i][7] ^ p[3][i+1][7]) ;
-	// 	else if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[0][i][7] ^ p[3][i+2][7]) ;
-	// 	else
-	// 		temp |= !(p[0][i][7]);
-	// }
-	// task &= temp;
-	// temp = bddfalse;
-	// for(unsigned i = 0; i < N; i++)
-	// {
-	// 	if(i != 2 && i != 5 && i != 8)
-	// 		temp |= !(p[0][i][5] ^ p[1][i+1][3]) ;
-	// 	else if(i == 1 || i == 2 || i == 4 || i == 5)
-	// 		temp |= !(p[0][i][5] ^ p[1][i+2][3]) ;
-	// 	else
-	// 		temp |= !(p[0][i][5]);
-	// }
-	// task &= temp;
+	//---------
 
+	//Границы значений параметров
 	bdd tmp = bddfalse;
 	for(unsigned i = 0; i < N; i++)
 	{
@@ -255,6 +218,7 @@ int main(void)
 		}
 	}
 
+	//Каждое значение - уникальное
 	for(unsigned i = 0; i < M; i++)
 	{
 		for(unsigned j = 0; j < N; j++)
